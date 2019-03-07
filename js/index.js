6c9bbc997cbd390204c635fc6893bcc4cb4abfe1
Chris Poulter <cpoulter@hotmail.co.uk>
2019-03-07 12:01:22 +0000

    removed bootstrap

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
-$(function() {
-    $(document).click(function() {
-        $('.navbar-collapse').collapse('hide');
-    });
-});
+// $(function() {
+//     $(document).click(function() {
+//         $('.navbar-collapse').collapse('hide');
+//     });
+// });
