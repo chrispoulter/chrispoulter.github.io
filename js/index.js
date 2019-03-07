64a87e093f4ac5b874bce309ecdac2cfc5b9d5b9
Chris Poulter <cpoulter@hotmail.co.uk>
2019-03-07 15:27:01 +0000

    added menu toggle

--- js/index.js
+++ js/index.js
@@ -1,5 +1,12 @@
-// $(function() {
-//     $(document).click(function() {
-//         $('.navbar-collapse').collapse('hide');
-//     });
-// });
+const menu = document.getElementById('menu');
+const toggler = document.getElementById('toggler');
+
+toggler.addEventListener('click', function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    menu.classList.toggle('collapsed');
+});
+
+window.addEventListener('click', function() {
+    menu.classList.add('collapsed');
+});
