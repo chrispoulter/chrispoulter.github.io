4dc068351fdc291d94eb0a361ae34741854c4005
Chris Poulter <cpoulter@hotmail.co.uk>
2019-05-09 13:22:52 +0100

    reverted styling

--- js/index.js
+++ js/index.js
@@ -1,12 +1,5 @@
-const menu = document.getElementById('menu');
-const toggler = document.getElementById('toggler');
-
-toggler.addEventListener('click', function(e) {
-    e.preventDefault();
-    e.stopPropagation();
-    menu.classList.toggle('collapsed');
-});
-
-window.addEventListener('click', function() {
-    menu.classList.add('collapsed');
+$(function() {
+    $(document).click(function() {
+        $('.navbar-collapse').collapse('hide');
+    });
 });
