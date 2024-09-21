473a92a464ba2fc6550c4ceef3138b166a01e362
Chris Poulter <cpoulter@hotmail.co.uk>
2024-09-21 12:35:53 +0100

    update config

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,15 +1,16 @@
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    root: 'src',
+    root: resolve(__dirname, 'src'),
     build: {
-        outDir: '../dist',
+        outDir: resolve(__dirname, 'dist'),
         emptyOutDir: true,
         rollupOptions: {
             input: {
-                main: './src/index.html',
-                404: './src/404.html',
-                500: './src/50x.html'
+                main: resolve(__dirname, 'src', 'index.html'),
+                404: resolve(__dirname, 'src', '404.html'),
+                500: resolve(__dirname, 'src', '50x.html')
             }
         }
     }
