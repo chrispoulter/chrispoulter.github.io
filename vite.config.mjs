a49c6f6934142d79e910ac8bc38f5b41635f8fc6
Chris Poulter <cpoulter@hotmail.co.uk>
2024-09-21 11:18:04 +0100

    use src folder

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+    root: 'src',
     build: {
+        outDir: '../dist',
+        emptyOutDir: true,
         rollupOptions: {
             input: {
-                main: './index.html',
-                404: './404.html',
-                500: './50x.html'
+                main: './src/index.html',
+                404: './src/404.html',
+                500: './src/50x.html'
             }
         }
     }
