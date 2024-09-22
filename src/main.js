1445d4e745ae72ba36e717b896ae93dd952aec81
Chris Poulter <cpoulter@hotmail.co.uk>
2024-09-22 12:29:41 +0100

    style nav links

--- src/main.js
+++ src/main.js
@@ -3,17 +3,17 @@ var navButton = document.getElementById('navButton');
 var navLinks = document.getElementById('navLinks');
 
 navButton.onclick = function () {
-    navLinks.classList.toggle('hidden');
-    navLinks.classList.toggle('flex');
+    // navLinks.classList.toggle('hidden');
+    // navLinks.classList.toggle('flex');
 };
 
 function clickOutsideNavBar(event) {
-    const isOutsideClick = !navBar.contains(event.target);
+    // const isOutsideClick = !navBar.contains(event.target);
 
-    if (isOutsideClick) {
-        navLinks.classList.add('hidden');
-        navLinks.classList.remove('flex');
-    }
+    // if (isOutsideClick) {
+    //     navLinks.classList.add('hidden');
+    //     navLinks.classList.remove('flex');
+    // }
 }
 
 document.addEventListener('click', clickOutsideNavBar);
