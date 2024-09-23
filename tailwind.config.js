03cea8510857a499ff5d4117a22018255dd2d5cd
Chris Poulter <cpoulter@hotmail.co.uk>
2024-09-23 15:58:54 +0100

    Feature/tailwind refactor (#6)
    * use vite
    
    * move icons
    
    * update prettier config
    
    * use src folder
    
    * update vscode settings
    
    * add tailwind
    
    * tailwind header
    
    * rollback
    
    * tailwind all the things
    
    * tailwind all the things
    
    * remove old styles
    
    * add hover for links
    
    * update button styling
    
    * style nav links
    
    * style nav links
    
    * responsive layout
    
    * custom paper background
    
    * remove unused classes
    
    * update bdage color
    
    * update font sizes
    
    * update icon sizes
    
    * code clean up
    
    * update colors
    
    * update icon focus
    
    * color clean up
    
    * update font color
    
    * update line height
    
    * update menu colors
    
    * remove custom values
    
    * remove sizing
    
    * update font sizes
    
    * size all the things
    
    * style error pages
    
    * update twitter icon
    
    * implement responsive navbar
    
    * update mobile spacing
    
    * update paper pattern
    
    * update header color
    
    * toggle button size
    
    * update header font size
    
    * update header font size
    
    * update spacing
    
    * update error pages

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    darkMode: 'class',
+    content: ['./src/**/*.{html,js,ts,jsx,tsx}'],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Open Sans', ...defaultTheme.fontFamily.sans]
+            },
+            fontSize: {
+                base: '.9rem'
+            }
+        }
+    },
+    plugins: []
+};
