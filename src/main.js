617248f9ec00ed63223a3b8f714fa810f27a93b5
Chris Poulter <cpoulter@hotmail.co.uk>
2024-09-23 14:37:49 +0100

    implement responsive navbar

--- src/main.js
+++ src/main.js
@@ -3,16 +3,16 @@ var navButton = document.getElementById('navButton');
 var navLinks = document.getElementById('navLinks');
 
 navButton.onclick = function () {
-    // navLinks.classList.toggle('hidden');
-    // navLinks.classList.toggle('flex');
+    navLinks.classList.toggle('hidden');
+    navLinks.classList.toggle('flex');
 };
 
 function clickOutsideNavBar(event) {
-    // const isOutsideClick = !navBar.contains(event.target);
-    // if (isOutsideClick) {
-    //     navLinks.classList.add('hidden');
-    //     navLinks.classList.remove('flex');
-    // }
+    const isOutsideClick = !navBar.contains(event.target);
+    if (isOutsideClick) {
+        navLinks.classList.add('hidden');
+        navLinks.classList.remove('flex');
+    }
 }
 
 document.addEventListener('click', clickOutsideNavBar);
