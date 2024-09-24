fd76b771c3220534af65f11fe7a77fa1a7b8528d
Chris Poulter <cpoulter@hotmail.co.uk>
2024-09-24 12:08:25 +0100

    add jumbotron

--- src/main.js
+++ src/main.js
@@ -4,14 +4,12 @@ var navLinks = document.getElementById('navLinks');
 
 navButton.onclick = function () {
     navLinks.classList.toggle('hidden');
-    navLinks.classList.toggle('flex');
 };
 
 function clickOutsideNavBar(event) {
     const isOutsideClick = !navBar.contains(event.target);
     if (isOutsideClick) {
         navLinks.classList.add('hidden');
-        navLinks.classList.remove('flex');
     }
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,12 @@ import defaultTheme from 'tailwindcss/defaultTheme';
 
 /** @type {import('tailwindcss').Config} */
 export default {
-    darkMode: 'class',
+    darkMode: 'media',
     content: ['./src/**/*.{html,js,ts,jsx,tsx}'],
     theme: {
         extend: {
             fontFamily: {
                 sans: ['Open Sans', ...defaultTheme.fontFamily.sans]
-            },
-            fontSize: {
-                base: '.9rem'
             }
         }
     },
