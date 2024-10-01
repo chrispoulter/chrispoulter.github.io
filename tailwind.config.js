77f24ae62325ba512311ffec74b364b553c37eeb
Chris Poulter <cpoulter@hotmail.co.uk>
2024-10-01 15:54:04 +0100

    add theme toggle button

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ import defaultTheme from 'tailwindcss/defaultTheme';
 
 /** @type {import('tailwindcss').Config} */
 export default {
-    darkMode: 'media',
+    darkMode: 'selector',
     content: ['./src/**/*.{html,js,ts,jsx,tsx}'],
     theme: {
         extend: {
