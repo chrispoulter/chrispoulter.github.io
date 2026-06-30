542198bfcfd0afc5f08ad5cdf61acc10894f9371
Chris Poulter <cpoulter@hotmail.co.uk>
2026-06-30 11:18:36 +0100

    update dependencies and enhance footer with dynamic version and year

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,30 @@
-import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
+import path from 'node:path';
+
+const gitCommitSha =
+    process.env.VITE_VERCEL_GIT_COMMIT_SHA ||
+    process.env.GIT_COMMIT_SHA ||
+    undefined;
+
+const version = gitCommitSha?.slice(0, 7) ?? process.env.npm_package_version;
+
+const currentYear = new Date().getFullYear();
 
 export default defineConfig({
+    define: {
+        'import.meta.env.VITE_APP_VERSION': JSON.stringify(version),
+        'import.meta.env.VITE_APP_BUILD_YEAR': JSON.stringify(currentYear)
+    },
     root: 'src',
     build: {
-        outDir: resolve(__dirname, 'dist'),
+        outDir: path.resolve(__dirname, 'dist'),
         emptyOutDir: true,
         rollupOptions: {
             input: {
-                main: resolve(__dirname, 'src', 'index.html'),
-                404: resolve(__dirname, 'src', '404.html'),
-                500: resolve(__dirname, 'src', '50x.html')
+                main: path.resolve(__dirname, 'src', 'index.html'),
+                404: path.resolve(__dirname, 'src', '404.html'),
+                500: path.resolve(__dirname, 'src', '50x.html')
             }
         }
     },
